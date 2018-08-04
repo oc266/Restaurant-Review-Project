b7637b9d6f2905dc6e9c5ac8116cd85fec6b6e77
Oliver Critchfield <oliver.critchfield@gmail.com>
2018-08-04 14:19:58 +0100

    Improved site a11y

--- js/main.js
+++ js/main.js
@@ -161,6 +161,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = restaurant.name;
   li.append(image);
 
   const name = document.createElement('h1');
@@ -178,6 +179,7 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
+  more.setAttribute('aria-label', `View resturant details for ${resta urant.name}`);
   li.append(more)
 
   return li
